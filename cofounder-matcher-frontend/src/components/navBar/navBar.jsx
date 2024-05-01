8d065218f8578cee8de55e071644a8237c4eb9ce
Josh Logan <joshlogan103@gmail.com>
2024-05-01 17:37:26 -0400

    Fix small bugs in app.jsx and navbar

--- cofounder-matcher-frontend/src/components/navBar/navBar.jsx
+++ cofounder-matcher-frontend/src/components/navBar/navBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navBar.css';
 import { removeToken } from '../../services/tokenServices';
@@ -19,27 +19,27 @@ const Navbar = () => {
     }
 
     return (
-      <Flex gap="3" justify="end">
-        <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
-            <DropdownMenu.Trigger asChild>
-                <Button className="dropdown-trigger">Menu &#9776;</Button>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content className="dropdown-content">
-                {['/', '/my-profile', '/profile-search', '/conversations', '/favorites'].map((path, index) => (
-                    <DropdownMenu.Item key={index} className="dropdown-menu-item">
-                        <NavLink to={path} className="nav-link" onClick={handleClick}>
-                            {path === '/' ? 'Home' :
-                            path === '/my-profile' ? 'My Profile' :
-                            path === '/profile-search' ? 'Search Profiles' :
-                            path === '/conversations' ? 'Inbox' :
-                            path === '/favorites' ? 'Favorites' :
-                            'Home'}
-                        </NavLink>
-                    </DropdownMenu.Item>
-                ))}
-                <Button className='logout' onClick={logOut}>Log Out</Button>
-            </DropdownMenu.Content>
-        </DropdownMenu.Root>
+        <Flex gap="3" justify="end">
+            <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
+                <DropdownMenu.Trigger asChild>
+                    <Button className="dropdown-trigger">Menu &#9776;</Button>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content className="dropdown-content">
+                    {['/', '/my-profile', '/profile-search', '/conversations', '/favorites'].map((path, index) => (
+                        <DropdownMenu.Item key={index} className="dropdown-menu-item">
+                            <NavLink to={path} className="nav-link" onClick={handleClick}>
+                                {path === '/' ? 'Home' :
+                                path === '/my-profile' ? 'My Profile' :
+                                path === '/profile-search' ? 'Search Profiles' :
+                                path === '/conversations' ? 'Inbox' :
+                                path === '/favorites' ? 'Favorites' :
+                                'Home'}
+                            </NavLink>
+                        </DropdownMenu.Item>
+                    ))}
+                    <Button className='logout' onClick={logOut}>Log Out</Button>
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
         </Flex>
     )
 }
