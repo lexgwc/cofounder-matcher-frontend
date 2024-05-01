ac537d8e0a8ad076c3b2ef55fdeadc5884aa6bc3
Josh Logan <joshlogan103@gmail.com>
2024-05-01 15:51:03 -0400

    Add initial version of create account page with api call working to update db

--- cofounder-matcher-frontend/src/pages/CreateAccount/CreateAccount.jsx
+++ cofounder-matcher-frontend/src/pages/CreateAccount/CreateAccount.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+// Import functions and styles
+
+import './CreateAccount.css'
+import { useState } from 'react'
+import { signup } from '../../services/apiServices.js'
+import { useNavigate } from 'react-router'
+
+// Import Radix components
+
+import { Flex, Box, Text, TextField, Button, Container } from '@radix-ui/themes'
+
+// Create Account Page
 
 const CreateAccount = () => {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log({email: email, password: password})
+  
+    try {
+      const apiResponse = await signup({ email: email, password: password })
+      if (apiResponse.status != 200) {
+        throw Error(apiResponse.error)
+      }
+      navigate('/login')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <div>CreateAccount</div>
+    <div>
+      <Flex>
+        <Box>
+          <Flex direction={'column'} gap={'2'} align={'center'} justify={'center'} width={'100vw'}>
+            <Container pt={'7'} align={'center'}>
+              <Text size={'7'}>Create an Account</Text>
+            </Container>
+            <form onSubmit={handleSubmit}>
+              <Flex direction={'column'} gap={'3'} pt={'5'}>
+                  <Text>Email</Text>
+                  <TextField.Root 
+                    type='text'
+                    value={email}
+                    onChange={e => { setEmail(e.target.value) }}
+                  />
+                  <Text>Password</Text>
+                  <TextField.Root 
+                    type='password'
+                    value={password}
+                    onChange={e => { setPassword(e.target.value) }}
+                  />
+                  <Button mt={'5'} type={'submit'}>Submit</Button>
+              </Flex>
+            </form>
+          </Flex>
+          
+        </Box>
+      </Flex>
+    </div>
   )
 }
 
--- cofounder-matcher-frontend/src/services/apiServices.js
+++ cofounder-matcher-frontend/src/services/apiServices.js
@@ -3,153 +3,153 @@ import { api } from './apiConnection.js'
 // User - API calls
 
 export const getUsers = async () => {
-  api.get('/users')
+  return await api.get('/users')
 }
 
 export const getUserById = async (id) => {
-  api.get(`/users/${id}`)
+  return await api.get(`/users/${id}`)
 }
 
 export const createUser = async (payload) => {
-  api.post('/users', payload)
+  return await api.post('/users', payload)
 }
 
 export const updateUserById = async (id, payload) => {
-  api.put(`/users/${id}`, payload)
+  return await api.put(`/users/${id}`, payload)
 }
 
 export const deleteUserById = async (id) => {
-  api.delete(`/users/${id}`)
+  return await api.delete(`/users/${id}`)
 }
 
 // Auth - API calls
 
-export const signup = (payload) => {
-  api.post(`/auth/signup`, payload)
+export const signup = async (payload) => {
+  return await api.post(`/auth/signup`, payload)
 }
 
-export const login = (payload) => {
-  api.post(`/auth/login`, payload)
+export const login = async (payload) => {
+  return await api.post(`/auth/login`, payload)
 }
 
-export const verifyLoggedIn = () => {
-  api.post(`/auth/verifyLoggedIn`)
+export const verifyLoggedIn = async () => {
+  return await api.post(`/auth/verifyLoggedIn`)
 }
 
 // Conversations - API calls
 
 export const getConversations = async () => {
-  api.get('/conversations')
+  return await api.get('/conversations')
 }
 
 export const getConversationById = async (id) => {
-  api.get(`/conversations/${id}`)
+  return await api.get(`/conversations/${id}`)
 }
 
 export const createConversation = async (payload) => {
-  api.post('/conversations', payload)
+  return await api.post('/conversations', payload)
 }
 
 export const updateConversationById = async (id, payload) => {
-  api.put(`/conversations/${id}`, payload)
+  return await api.put(`/conversations/${id}`, payload)
 }
 
 export const deleteConversationById = async (id) => {
-  api.delete(`/conversations/${id}`)
+  return await api.delete(`/conversations/${id}`)
 }
 
 // Messages - API calls
 
 export const getMessages = async () => {
-  api.get('/messages')
+  return await api.get('/messages')
 }
 
 export const getMessageById = async (id) => {
-  api.get(`/messages/${id}`)
+  return await api.get(`/messages/${id}`)
 }
 
 export const createMessageAndCreateConversation = async (payload) => {
-  api.post('/messages/new-conversation', payload)
+  return await api.post('/messages/new-conversation', payload)
 }
 
 export const createMessageAndUpdateConversation = async (conversationId, payload) => {
-  api.post(`/messages/update-conversation/${conversationId}`, payload)
+  return await api.post(`/messages/update-conversation/${conversationId}`, payload)
 }
 
 export const updateMessageById = async (id, payload) => {
-  api.put(`/messages/${id}`, payload)
+  return await api.put(`/messages/${id}`, payload)
 }
 
 export const deleteMessageById = async (id) => {
-  api.delete(`/messages/${id}`)
+  return await api.delete(`/messages/${id}`)
 }
 
 // Profile - API calls
 
 export const getProfiles = async () => {
-  api.get('/profiles')
+  return await api.get('/profiles')
 }
 
 export const getProfileById = async (id) => {
-  api.get(`/profiles/${id}`)
+  return await api.get(`/profiles/${id}`)
 }
 
 export const createProfile = async (payload) => {
-  api.post('/profiles/', payload)
+  return await api.post('/profiles/', payload)
 }
 
 export const updateProfileById = async (id, payload) => {
-  api.put(`/profiles/${id}`, payload)
+  return await api.put(`/profiles/${id}`, payload)
 }
 
 export const deleteProfileById = async (id) => {
-  api.delete(`/profiles/${id}`)
+  return await api.delete(`/profiles/${id}`)
 }
 
 // Schools - API calls
 
 export const getSchools = async () => {
-  api.get('/schools')
+  return await api.get('/schools')
 }
 
 export const getSchoolById = async (id) => {
-  api.get(`/schools/${id}`)
+  return await api.get(`/schools/${id}`)
 }
 
 export const createSchool = async (payload) => {
-  api.post('/schools/', payload)
+  return await api.post('/schools/', payload)
 }
 
 export const updateSchoolById = async (id, payload) => {
-  api.put(`/schools/${id}`, payload)
+  return await api.put(`/schools/${id}`, payload)
 }
 
 export const deleteSchoolById = async (id) => {
-  api.delete(`/schools/${id}`)
+  return await api.delete(`/schools/${id}`)
 }
 
 // Favorites - API calls
 
 export const getFavorites = async () => {
-  api.get('/favorites')
+  return await api.get('/favorites')
 }
 
 export const getFavoriteById = async (id) => {
-  api.get(`/favorites/${id}`)
+  return await api.get(`/favorites/${id}`)
 }
 
 export const getAllFavoritesByUserId = async (userId) => {
-  api.get(`/favorites/my-favorites/${userId}`)
+  return await api.get(`/favorites/my-favorites/${userId}`)
 }
 
 export const createFavorite = async (payload) => {
-  api.post('/favorites/', payload)
+  return await api.post('/favorites/', payload)
 }
 
 export const updateFavoriteById = async (id, payload) => {
-  api.put(`/favorites/${id}`, payload)
+  return await api.put(`/favorites/${id}`, payload)
 }
 
 export const deleteFavoriteById = async (id) => {
-  api.delete(`/favorites/${id}`)
+  return await api.delete(`/favorites/${id}`)
 }
