ce577ff37fff7c9e4c59ab958e27da5c2c624d49
Alexa Clark <lex.gwc@gmail.com>
2024-05-01 13:29:11 -0400

    Update filters

--- cofounder-matcher-frontend/src/components/navBar/navBar.jsx
+++ cofounder-matcher-frontend/src/components/navBar/navBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navBar.css';
-import { DropdownMenu, Button } from '@radix-ui/themes'
+import { DropdownMenu, Button, Flex } from '@radix-ui/themes'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,25 +11,29 @@ const Navbar = () => {
     }
 
     return (
-        <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
-            <DropdownMenu.Trigger asChild>
-                <Button className="dropdown-trigger">Menu &#9776;</Button>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content className="dropdown-content">
-                {['/', '/my-profile', '/profile-search', '/conversations', '/favorites'].map((path, index) => (
-                    <DropdownMenu.Item key={index} className="dropdown-menu-item">
-                        <NavLink to={path} className="nav-link" onClick={handleClick}>
-                            {path === '/' ? 'Home' :
-                            path === '/my-profile' ? 'My Profile' :
-                            path === '/profile-search' ? 'Search Profiles' :
-                            path === '/conversations' ? 'Inbox' :
-                            path === '/favorites' ? 'Favorites' :
-                            'Home'}
-                        </NavLink>
-                    </DropdownMenu.Item>
-                ))}
-            </DropdownMenu.Content>
-        </DropdownMenu.Root>
+        <Flex gap="3" justify="end">
+            <>
+                <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
+                    <DropdownMenu.Trigger asChild>
+                        <Button className="dropdown-trigger"> &#9776;</Button>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content className="dropdown-content">
+                        {['/', '/my-profile', '/profile-search', '/conversations', '/favorites'].map((path, index) => (
+                            <DropdownMenu.Item key={index} className="dropdown-menu-item">
+                                <NavLink to={path} className="nav-link" onClick={handleClick}>
+                                    {path === '/' ? 'Home' :
+                                    path === '/my-profile' ? 'My Profile' :
+                                    path === '/profile-search' ? 'Search Profiles' :
+                                    path === '/conversations' ? 'Inbox' :
+                                    path === '/favorites' ? 'Favorites' :
+                                    'Home'}
+                                </NavLink>
+                            </DropdownMenu.Item>
+                        ))}
+                    </DropdownMenu.Content>
+                </DropdownMenu.Root>
+            </>
+        </Flex>
     );
 }
 
